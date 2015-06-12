c3758c071a1cb40bc0be9d872eac568cb16c1dea
Selene <hakobyan.sn@gmail.com>
2015-06-11 22:31:42 -0700

    user update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, except: [:show]
+    before_action :authenticate_user!
 
     def show
       @user = User.find(params[:id])
+    end
+
+    def update
+      if current_user.update_attributes(user_params)
+       flash[:notice] = "User information updated"
+       redirect_to edit_user_registration_path
+      else
+       flash[:error] = "Invalid user information"
+       redirect_to edit_user_registration_path
+      end
     end
 
     private
