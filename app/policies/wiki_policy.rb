4910ca1826228184a83cf9ed78fd17f7608e7fb0
Selene <hakobyan.sn@gmail.com>
2015-06-22 23:02:07 -0700

    heroku

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -19,7 +19,7 @@ class WikiPolicy < ApplicationPolicy
       if user && (user.admin? || user.premium?)
         scope.all
       elsif user
-        scope.where(private: false || private: nil)
+        scope.where(private: false || nil)
       end
     end
   end
